b2ffd05e9ee50f36b3a8630008e55b7c83f87f48
lixiaojin123 <20141105062l@FanydeiMac13.local>
2016-06-20 10:12:32 +0800

    常量和变量
    用swift语言输出hello lxj ，只需要打开Xcode 创建一个OS X－Application-Command line
    TOOL，把word改为lx

---
+++ main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  ios1
+//
+//  Created by 李晓晋 on 16/6/20.
+//  Copyright © 2016年 李晓晋. All rights reserved.
+//
+
+import Foundation
+
+var  a = 3
+a = 6
+var  b = 10
+
+let c = a+b
+
+print (c)
